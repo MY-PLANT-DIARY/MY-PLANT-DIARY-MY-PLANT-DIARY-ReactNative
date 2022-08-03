a30d4aeb2ca7a0c083b930a21c930b9a1af4d824
김예림 <72791856+yr0118kim@users.noreply.github.com>
2022-08-04 01:36:31 +0900

    Add files via upload

---
+++ App.js
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import MainContainer from './navigation/MainContainer';
+import {View,Text} from 'react-native';
+
+export function App() {
+  return(
+    <Text>Hello</Text>,
+    <MainContainer/>
+  );
+}
+
+export default App;
